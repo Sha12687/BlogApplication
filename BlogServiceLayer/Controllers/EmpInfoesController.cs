fc60c4010159c41c98a0463192bcd74c53206b6d
Shashant <shashantchaudhary999@gmail.com>
2023-12-17 10:49:30 +0530

    EmpInfoserviceLayerComplete

--- BlogServiceLayer/Controllers/EmpInfoesController.cs
+++ BlogServiceLayer/Controllers/EmpInfoesController.cs
@@ -7,12 +7,14 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using System.Web.Http.Description;
 using BlogDAL;
 using BlogServiceLayer.Models;
 
 namespace BlogServiceLayer.Controllers
 {
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
     public class EmpInfoesController : ApiController
     {
         private BlogDbContext db = new BlogDbContext();
